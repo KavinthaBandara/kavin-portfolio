c297096e482694d546198d60d915a4a0e12c7a15
KavinthaBandara <kavinthahasaranga2@gmail.com>
2026-06-15 09:12:42 -0700

    made changes to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50  dark:bg-black font-light font-sans">
         <p className="max-w-4xl mx-auto text-center text-lg leading-relaxed text-gray-800 dark:text-gray-200">
-          Hi, I’m Kavintha Bandara, a passionate AI engineer and web developer dedicated to building intelligent, user-friendly digital experiences. I thrive at the intersection of cutting-edge technology and creative problem-solving, exploring how artificial intelligence can enhance web applications and transform the way we interact with technology.
+          <span className="font-bold text-2xl">Hi,</span> I’m Kavintha Bandara, a passionate AI engineer and web developer dedicated to building intelligent, user-friendly digital experiences. I thrive at the intersection of cutting-edge technology and creative problem-solving, exploring how artificial intelligence can enhance web applications and transform the way we interact with technology.
           Whether it’s designing dynamic websites, developing AI-powered tools, or experimenting with new frameworks and algorithms, I love turning complex ideas into practical, elegant solutions. Explore my portfolio to see projects that showcase my skills, curiosity, and commitment to innovation.
             
         </p>
