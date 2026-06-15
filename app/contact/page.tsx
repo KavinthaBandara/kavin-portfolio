1a30b35e7266bf18cfd42d8a9849ffd4a1a2c3e8
KavinthaBandara <kavinthahasaranga2@gmail.com>
2026-06-15 09:57:39 -0700

    fixed many errors

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -11,7 +11,7 @@ export default function Contact() {
 
     <div className="text-center">
         <h1 className="text-4xl font-bold mb-4 dark:text-gray-100 mb-2">Contact Me</h1>
-        <p className="text-black-400 dark:text-gray-300">
+        <p className="text-white-400 white:text-gray-300">
             Get in touch with me!
         </p>
     </div>
@@ -29,26 +29,30 @@ export default function Contact() {
         </div>
 
         <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300">
-            <h2 className="text-xl font-semibold mb-2">Email 2</h2>
-            <p className="text-black-300 ">kavintha.bandara01@gmail.com</p>
+            <h2 className="text-xl font-semibold text-gray-900 dark:text-gray-100 mb-2">
+                Email 2
+            </h2>
+            <p className="text-gray-600 dark:text-gray-300">
+                kavintha.bandara01@gmail.com
+            </p>
         </div>
 
 
-        <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300">
-            <h2 className="text-xl font-semibold mb-2">Phone</h2>
-            <p className="text-black-300">+94 76 451 8970</p>
+        <div className="bg-white dark:bg-gray-900 rounded-lg shadow-md p-6 hover:shadow-xl transition-shadow duration-300">
+            <h2 className="text-xl font-semibold text-gray-900 dark:text-gray-100 mb-2">Phone</h2>
+            <p className="text-gray-600 dark:text-gray-300">+94 76 451 8970</p>
         </div>
 
   
-        <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300">
-            <h2 className="text-xl font-semibold mb-2">LinkedIn</h2>
+        <div className="bg-white dark:bg-gray-900 rounded-lg shadow-md p-6 hover:shadow-xl transition-shadow duration-300">
+            <h2 className="text-xl font-semibold text-gray-900 dark:text-gray-100 mb-2">LinkedIn</h2>
             <a href="https://www.linkedin.com/in/kavintha-bandara-6a0304339" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
                 linkedin.com/in/kavintha-bandara-6a0304339
             </a>
         </div>
 
-        <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300">
-            <h2 className="text-xl font-semibold mb-2">Instagram</h2>
+        <div className="bg-white dark:bg-gray-900 rounded-lg shadow-md p-6 hover:shadow-xl transition-shadow duration-300">
+            <h2 className="text-xl font-semibold text-gray-900 dark:text-gray-100 mb-2">Instagram</h2>
             <a href="https://www.instagram.com/bandara_x0/" target="_blank" rel="noopener noreferrer" className="text-pink-500 hover:underline ">
                 instagram.com/bandara_x0
             </a>
